42031c30637d9d39f1d392ff68704f5ee2e8d8e8
Tony Smolenski <tsmo@mac.com>
2014-06-13 22:52:48 -0400

    Committing_answer_June13

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is comprised of 2 functions.
+## MakeCacheMatrix coverts an inputted matrix into a "cached matrix", which 
+## can "house" the inverse of itself after it's Solved.
 
-## Write a short comment describing this function
+## This first function will convert a given matrix, into a "cached matrix".
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        imtx <- NULL
+        set <- function(y) {
+                x <<- y
+                imtx <<- NULL
+        }
+        get <- function() x
+        setimtx <- function(solve) imtx <<- solve
+        getimtx <- function() imtx
+        list(set = set, get = get,
+             setimtx = setimtx,
+             getimtx = getimtx)
 }
 
 
-## Write a short comment describing this function
+## This function takes a cached matrix as an input, checks if it already 
+## "houses" it's inverse.  If it does then it prints it.  If it doesn't, it will
+## Solve for it, and then return it to the Cached matrix for future use.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        imtx <- x$getimtx()
+        if(!is.null(imtx)) {
+                message("getting cached data")
+                return(imtx)
+        }
+        data <- x$get()
+        imtx <- solve(data, ...)
+        x$setimtx(imtx)
+        imtx
 }
